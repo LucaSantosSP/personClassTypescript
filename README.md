c5de0f766ba5201e3d5926af67db428e83d8ec8d
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-08-26 21:25:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-rv3wfj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-rv3wfj)
---
+++ person.ts
@@ -0,0 +1,12 @@
+class Person{
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  age: number;
+
+  constructor(firstName: string, middleName:string, lastName: string, age: number){
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.age = age;
+}
