689d1b69a1f4ec31dbf691f6d03fca6e283e9d12
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-08-26 21:31:04 -0300

    Criada a classe Person.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
-// Import stylesheets
-import './style.css';
-
-// Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
--- person.ts
+++ person.ts
@@ -1,12 +1,22 @@
-class Person{
+class Person {
   firstName: string;
   middleName: string;
   lastName: string;
   age: number;
 
-  constructor(firstName: string, middleName:string, lastName: string, age: number){
+  constructor(
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    age: number
+  ) {
     this.firstName = firstName;
     this.middleName = middleName;
     this.lastName = lastName;
     this.age = age;
+  }
+
+  getFullName() {
+    return this.firstName + ' ' + this.lastName + ' ' + this.lastName;
+  }
 }
